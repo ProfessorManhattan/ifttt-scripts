4cf1dcf4ba6c57be644e7143880e247b570d4160
Megabyte Labs <help@megabyte.space>
2022-06-09 00:13:15 -0400

    ☁️ chore(automation): Applying changes from upstream repository.

--- scripts/chinese-youtube-cf.js
+++ scripts/chinese-youtube-cf.js
@@ -1,47 +1,47 @@
 // Deploy this to a CF worker after launching
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request).catch((error: unknown) => new Response(error.stack, { status: 500 })))
+})
 
 /**
  * Parse the response from the Heroku app
+ *
+ * @param response
  */
 async function gatherResponse(response) {
-  const { headers } = response;
-  const contentType = headers.get('content-type') || '';
+  const { headers } = response
+  const contentType = headers.get('content-type') || ''
   if (contentType.includes('application/json')) {
-    return JSON.stringify(await response.json());
+    return JSON.stringify(await response.json())
   } else if (contentType.includes('application/text')) {
-    return response.text();
+    return response.text()
   } else if (contentType.includes('text/html')) {
-    return response.text();
-  } else {
-    return response.text();
+    return response.text()
   }
+
+  return response.text()
 }
 
 /**
  * Many more examples available at:
  *   https://developers.cloudflare.com/workers/examples
+ *
  * @param {Request} request
  * @returns {Promise<Response>}
  */
 async function handleRequest(request) {
   const url = new URL(request.url)
-  const params = new Proxy(new URLSearchParams(url.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-const tubeUrl = params.url;
-    const youTubeDlData = await fetch('https://chinese-youtube.herokuapp.com/api/info?url=' + tubeUrl, {
+  const parameters = new Proxy(new URLSearchParams(url.search), {
+    get: (searchParameters, property) => searchParameters.get(property)
+  })
+  const tubeUrl = parameters.url
+  const youTubeDlData = await fetch(`https://chinese-youtube.herokuapp.com/api/info?url=${tubeUrl}`, {
     headers: {
-      'content-type': 'application/json;charset=UTF-8',
-    },
-  });
-    const results = await gatherResponse(youTubeDlData);
-    const playUrl = JSON.parse(results).info.formats[0].url
-    return fetch(playUrl);
+      'content-type': 'application/json;charset=UTF-8'
+    }
+  })
+  const results = await gatherResponse(youTubeDlData)
+  const playUrl = JSON.parse(results).info.formats[0].url
+
+  return fetch(playUrl)
 }
